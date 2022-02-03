4dcc2603f33962a26ab07adcf66ee9f5f2674320
yassine <yassineboulakhrif75@gmail.com>
2022-02-03 14:28:11 +0100

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
-const items = document.querySelectorAll('.item')
 const image = document.querySelector('img')
+const items = document.querySelectorAll('.item')
 
 items.forEach((el) => {
   el.addEventListener('mouseover', (e) => {
+
     imageData = e.target.getAttribute('data-image')
     console.log(imageData)
     e.target.style.zIndex = 99
     image.setAttribute('src', imageData)
+
   })
   el.addEventListener('mousemove', (e) => {
+
     image.style.top = e.clientY + 'px'
     image.style.left = e.clientX + 'px'
   })
   el.addEventListener('mouseleave', (e) => {
+
     e.target.style.zIndex = 1
     image.setAttribute('src', '')
   })
